309f7f8a9a8c0db490b7cca701671e9fa4ec8f73
unknown <markchovava@gmail.com>
2025-10-10 16:19:45 +0200

    Working

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['127.0.0.1'],
+    domains: ['127.0.0.1', 'www.fldesigners.xyz', 'fldesigners.xyz'],
   },
   experimental: {
     serverActions: {
