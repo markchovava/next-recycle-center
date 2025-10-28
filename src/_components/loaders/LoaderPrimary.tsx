0fd2f044ec4fbd7e596e3529cc371d9b80700723
unknown <markchovava@gmail.com>
2025-10-28 13:27:18 +0200

    A

--- src/_components/loaders/LoaderPrimary.tsx
+++ src/_components/loaders/LoaderPrimary.tsx
@@ -4,7 +4,7 @@ export default function LoaderPrimary(){
     return (
         <div className={`text-center py-12 flex items-center justify-center bg-white drop-shadow px-6 rounded-xl `}>
              <p className="text-center font-light text-3xl">Loading...</p>
-           </div>
+        </div>
 
     )
 }
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function loading() {
   return (
-    <div>loading...</div>
+    <div className='h-[28rem] text-[2.4rem] w-full flex items-center justify-center'>
+      loading...
+    </div>
   )
 }
